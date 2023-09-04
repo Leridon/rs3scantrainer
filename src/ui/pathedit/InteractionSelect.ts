3431651c714bfd65b2900015605dd9015d02e7ec
Lukas Gail <lukas.gail@outlook.com>
2023-09-04 13:42:29 +0200

    Fix typing issue with interaction types

--- src/ui/pathedit/InteractionSelect.ts
+++ src/ui/pathedit/InteractionSelect.ts
@@ -1,15 +1,15 @@
 import {DropdownSelection} from "../widgets/DropdownSelection";
 import {Path} from "../../model/pathing";
 
-export default class InteractionSelect extends DropdownSelection<Path.InteractionType.Enum> {
+export default class InteractionSelect extends DropdownSelection<Path.InteractionType> {
     constructor() {
         super({
             type_class: {
-                toHTML: (v: Path.InteractionType.Enum) => {
+                toHTML: (v: Path.InteractionType) => {
                     let meta = Path.InteractionType.meta(v)
                     return c(`<div><img style="height: 18px; object-fit: contain; margin-right: 3px" src="${meta.icon_url}" alt="${meta.description}">${meta.description}</div>`)
                 }
             }
-        }, Object.values(Path.InteractionType.Enum) as Path.InteractionType.Enum[]);
+        }, Path.InteractionType.all());
     }
 }

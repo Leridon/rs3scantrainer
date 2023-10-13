aae744fc503425ae99d292252eb6d9269b6d509a
Lukas Gail <lukas.gail@outlook.com>
2023-10-13 17:30:41 +0200

    Reduce size of template string edit

--- src/ui/widgets/TemplateStringEdit.ts
+++ src/ui/widgets/TemplateStringEdit.ts
@@ -5,10 +5,12 @@ import SmallImageButton from "./SmallImageButton";
 
 export default class TemplateStringEdit extends AbstractEditWidget<string> {
 
-    input_row: Widget
-    instruction_input: Widget
+    edit_mode: boolean = false
 
-    render_view: Widget
+    main_row: Widget = null
+    preview_container: Widget
+
+    instruction_input: Widget = null
 
     constructor(private options: {
         resolver: TemplateResolver,
@@ -22,38 +24,80 @@ export default class TemplateStringEdit extends AbstractEditWidget<string> {
     }
 
     protected render() {
+        this.empty()
+        this.instruction_input = null
+        this.preview_container = null
 
-        this.input_row = c("<div class='ctr-template-string-edit-input-row'></div>").appendTo(this)
+        this.main_row = c("<div class='ctr-template-string-edit-input-row'></div>")
 
-        this.instruction_input = c("<input type='text' class='nisinput'>")
-            .tapRaw(r => r
-                .on("input", () => {
-                    this.value = this.instruction_input.container.val() as string
-                    // Only update preview without immediately triggering the change
-                    this.renderPreview()
-                })
-                .on("change", () => {
-                    this.changed(this.instruction_input.container.val() as string)
-                }))
-            .appendTo(this.input_row)
+        let generate_btn = SmallImageButton.new("assets/icons/regenerate.png")
+            .css("margin-left", "2px")
+            .tooltip("Auto generate")
+            .setEnabled(!!this.options.generator)
+            .on("click", () => {
+                this.setValue(this.options.generator())
+                this.changed(this.value)
+            })
 
-        if (this.options.generator) {
-            SmallImageButton.new("assets/icons/regenerate.png")
+
+        if (this.edit_mode) {
+            this.instruction_input = c("<input type='text' class='nisinput'>")
+                .tapRaw(r => r
+                    .val(this.value)
+                    .on("input", () => {
+                        this.value = this.instruction_input.container.val() as string
+                        // Only update preview without immediately triggering the change
+                        this.renderPreview()
+                    })
+                    .on("change", () => {
+                        this.changed(this.instruction_input.container.val() as string)
+                    })
+                    .on("keyup", (e) => {
+                        if (e.key === 'Enter') this.instruction_input.raw().blur()
+                    })
+                    .on("focusout", () => {
+                        this.edit_mode = false
+
+                        this.render()
+                    })
+                )
+
+            this.main_row
+                .append(this.instruction_input)
+                .append(generate_btn)
+                .appendTo(this)
+
+            this.preview_container = c("<div style='padding-left: 5px'>").appendTo(this)
+
+        } else {
+            let edit_button = SmallImageButton.new("assets/icons/edit.png")
                 .css("margin-left", "2px")
-                .tooltip("Auto generate")
+                .tooltip("Edit")
+                .setEnabled(!!this.options.generator)
                 .on("click", () => {
-                    this.setValue(this.options.generator())
-                    this.changed(this.value)
-                }).appendTo(this.input_row)
-        }
+                    this.startEdit()
+                })
 
-        this.render_view = c("<div style='padding-left: 5px'>").appendTo(this)
+            this.preview_container = c("<span style='padding-left: 5px; cursor: pointer; flex-grow: 1'>")
+                .tooltip("Edit")
+                .tapRaw(r => r.on("click", () => {
+                    this.startEdit()
+                }))
+
+            this.main_row
+                .append(this.preview_container)
+                .append(edit_button)
+                .append(generate_btn)
+                .appendTo(this)
+        }
 
         this.renderPreview()
     }
 
     protected update() {
-        this.instruction_input.container.val(this.value)
+        if (this.instruction_input) {
+            this.instruction_input.container.val(this.value)
+        }
         this.renderPreview()
     }
 
@@ -65,6 +109,12 @@ export default class TemplateStringEdit extends AbstractEditWidget<string> {
     }
 
     private renderPreview() {
-        this.render_view.container.html(`Preview: ${this.options.resolver.resolve(this.value || "")}`)
+        this.preview_container.container.html(`${this.options.resolver.resolve(this.value || "")}`)
+    }
+
+    public startEdit() {
+        this.edit_mode = true
+        this.render()
+        this.instruction_input.container.focus()
     }
 }

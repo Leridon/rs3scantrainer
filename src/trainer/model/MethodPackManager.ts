5f37478d1af47ff7e0a7cc3adf331a2b4bf7fb85
Lukas Gail <lukas.gail@outlook.com>
2023-11-19 23:20:04 +0100

    More prototyping for the method manager

--- src/trainer/model/MethodPackManager.ts
+++ src/trainer/model/MethodPackManager.ts
@@ -1,27 +1,70 @@
 import {MethodPack} from "./MethodPack";
 import {SolvingMethods} from "./methods";
 import Method = SolvingMethods.Method;
-import {TileCoordinates} from "../../lib/runescape/coordinates";
+import {TileCoordinates, TileRectangle} from "../../lib/runescape/coordinates";
+import tr = TileRectangle.tr;
 
-type M = {
-    for: number | { spot: TileCoordinates },
-    method: Method
+type M_base = {
+    for: number | TileCoordinates,
+    name: string,
+    description: string,
+}
+
+type M = M_base
+
+type MethodInPack = {
+    id: number,
+    method: M
+}
+
+type MethodId = {
+    pack: number,
+    method: number
+}
+
+type Pack = {
+    id: number,
+    is_builtin: boolean,
+    author: string,
+    description: string,
+    timestamp: number,
+    methods: MethodInPack[]
+}
+
+type M2 = {
+    pack: Pack,
+    method: MethodInPack,
+    is_favourite: boolean
 }
 
 
 class MethodPackManager {
 
-    private pack_index: {}[]
+    private packs: Pack[] = []
+    private favourites: {clue: number | TileCoordinates, method: MethodId}[] = []
 
     // Save method packs in indexed db, localstorage is too small
 
-    get(): { is_local: boolean, pack: MethodPack }[] {
-
-
-        return null
+    all(): Pack[] {
+        return [...this.packs]
     }
 
-    createPack(name: String): MethodPack {
+    createPack(name: String): Pack {
+        let n: Pack = {
+            id: null,
+            is_builtin: false,
+            author: "",
+            description: "",
+            timestamp: 0,
+            methods: []
+        }
+
+        this.packs.push(n)
+
+        return n
+    }
+
+    importPack(string: string): Pack {
 
     }
 
@@ -40,11 +83,23 @@ class MethodPackManager {
     deleteMethod(pack_id: string, method_id: string) {}
 
 
-    setFavorite(clue_id: number, method_id: { pack: string, method: string }) {
+    setFavorite(method: M2) {
 
     }
 
-    getForClue(id: number): { id: { pack: string, method: string }, favourite: boolean, method: Method }[] {
-
+    getForClue(id: number | TileCoordinates): M2[] {
+        // TODO: Probably best to have an internal index as a lookup table
+        return this.packs.flatMap(p => p.methods.filter(m => id_match(m.method.for, id))
+            .map(m => ({
+                pack: p,
+                method: m,
+                is_favourite: false // TODO
+            }))
+        )
     }
+}
+
+function id_match(a: number | TileCoordinates, b: number | TileCoordinates): boolean {
+    // TODO!
+    return true
 }

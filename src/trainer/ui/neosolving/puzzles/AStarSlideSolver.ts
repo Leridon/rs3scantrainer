4934415a5c21558dbd100b3e9528ba76a4dafd62
Lukas Gail <lukas.gail@outlook.com>
2024-04-11 12:50:18 +0200

    Finish implementation of IDA*

--- src/trainer/ui/neosolving/puzzles/AStarSlideSolver.ts
+++ src/trainer/ui/neosolving/puzzles/AStarSlideSolver.ts
@@ -1,130 +1,265 @@
 import {Sliders} from "./Sliders";
-import * as lodash from "lodash";
+import {ewent} from "../../../../lib/reactive";
+import {delay} from "../../../../skillbertssolver/oldlib";
 import SliderState = Sliders.SliderState;
-import MoveList = Sliders.MoveList;
 import Move = Sliders.Move;
 
-export class AStarSlideSolver extends Sliders.SlideSolver {
-  protected async solve_implementation() {
-    type Node = Sliders.SliderState
+abstract class Process<Result> {
+  private finished_event = ewent<this>()
 
-    function heuristic(node: Node): number {
-      return SliderState.sumManhattenDistance(node) * 2
+  private is_running: boolean = false
+  protected should_stop: boolean = false
+  private finished: boolean = false
+
+  protected start_time: number
+  private last_interrupt_time: number
+  protected end_time: number
+  private progress: number
+
+  private timeout: number = Number.MAX_SAFE_INTEGER
+
+  withTimeout(timeout: number): this {
+    this.timeout = timeout
+    return this
+  }
+
+  private interrupt_ewent = ewent<null>()
+
+  abstract implementation(): Promise<Result> | Result
+
+  async run(): Promise<Result> {
+    if (this.is_running || this.finished) return null
+
+    this.is_running = true
+    this.should_stop = false
+
+    this.start_time = Date.now();
+    this.end_time = this.start_time + this.timeout;
+    this.last_interrupt_time = this.start_time
+
+    const result = await this.implementation()
+
+    this.is_running = false
+    this.should_stop = false
+    this.finished = true
+
+    return result
+  }
+
+  onInterrupt(f: () => void): this {
+    this.interrupt_ewent.on(f)
+    return this
+  }
+
+  protected async interrupt() {
+    this.interrupt_ewent.trigger(null)
+    await delay(1)
+  }
+
+  stop() {
+    if (!this.is_running) return
+
+    this.should_stop = true
+  }
+
+  await() {
+    return new Promise(resolve => {
+      this.finished_event.on(resolve)
+    })
+  }
+
+  protected async checkTime() {
+    const t = Date.now()
+
+    const INTERRUPT_FREQUENCY = 50
+
+    if (t >= this.end_time) this.stop()
+    else if (t >= this.last_interrupt_time + INTERRUPT_FREQUENCY) {
+      this.last_interrupt_time = t
+      await this.interrupt()
     }
+  }
+}
 
-    function is_goal(node: State): boolean {
+export class IterativeDeepeningAStar<UnderlyingState, Move> extends Process<IterativeDeepeningAStar.State<UnderlyingState, Move>> {
 
-      if (!node) debugger
-      return SliderState.equals(node.state, SliderState.SOLVED)
-    }
+  constructor(
+    private start_state: UnderlyingState,
+    private heuristic: (_: UnderlyingState) => number,
+    private successors: (_: IterativeDeepeningAStar.State<UnderlyingState, Move>) => IterativeDeepeningAStar.Edge<UnderlyingState, Move>[],
+    private isGoal: (_: UnderlyingState) => boolean
+  ) {
+    super()
+  }
 
-    function conta(state: State, config: State): boolean {
-      if (state == null) return false
-      if (SliderState.equals(state.state, config.state)) return true
-      return conta(state.parent?.state, config)
-    }
-
-    function successors(node: State): State[] {
-      const was_vertical = node.parent && node.parent.move % 5 == 0
-      const was_horizontal = node.parent && !was_vertical
-
-      const blank_x = node.blank % 5
-      const blank_y = Math.floor(node.blank / 5)
-
-      const successors: State[] = []
-
-      function child(move: Move) {
-        const state = SliderState.withMove(node.state, move)
-        successors.push({
-          parent: {state: node, move: move},
-          state: state,
-          depth: node.depth + 1,
-          blank: node.blank + move,
-          estimate: node.depth + 1 + heuristic(state)
-        })
+  private getSuccessors(state: IterativeDeepeningAStar.State<UnderlyingState, Move>): IterativeDeepeningAStar.State<UnderlyingState, Move>[] {
+    return this.successors(state).map((edge) => {
+      return {
+        parent: {state: state, move: edge.move},
+        state: edge.target,
+        length: state.length + edge.cost
       }
+    })
+  }
 
-      if (!was_horizontal) {
-        for (let xi = 0; xi < 5; xi++) {
-          if (xi != blank_x) {
-            child(blank_y * 5 + xi - node.blank)
-          }
-        }
-      }
+  private step(state: IterativeDeepeningAStar.State<UnderlyingState, Move>,
+               bound: number
+  ): [[IterativeDeepeningAStar.State<UnderlyingState, Move>, number], boolean] {
+    this.checkTime()
 
-      if (!was_vertical) {
-        for (let yi = 0; yi < 5; yi++) {
-          if (yi != blank_y) {
-            child(yi * 5 + blank_x - node.blank)
-          }
-        }
-      }
+    const h = this.heuristic(state.state)
+    const f = state.length + h
 
-      return successors//.filter(c => !conta(node, c))
+    if (f > bound || this.should_stop) {
+      return [[state, f], false]
     }
 
-    type State = {
-      parent: {
-        state: State,
-        move: number
-      } | null,
-      depth: number,
-      state: SliderState,
-      blank: number,
-      estimate: number
+    if (this.isGoal(state.state)) {
+      return [[state, f], true]
     }
 
-    const self = this
+    let min = Number.MAX_SAFE_INTEGER
 
-    let best: State = null
+    for (const child of this.getSuccessors(state)) {
+      const [[c, v], solved] = this.step(child, bound,)
 
-    function found(state: State) {
-      if (!best || state.depth < best.depth) {
-        best = state
-        self.registerSolution(reduce(state))
-      }
+      if (solved) return [[c, v], solved]
+
+      if (v < min) min = v
     }
 
-    function reduce(state: State): MoveList {
+    return [[null, min], false]
+  }
+
+  async implementation(): Promise<IterativeDeepeningAStar.State<UnderlyingState, Move>> {
+    const start_state: IterativeDeepeningAStar.State<UnderlyingState, Move> = {
+      parent: null,
+      length: 0,
+      state: this.start_state
+    }
+
+    let bound = this.heuristic(start_state.state)
+
+    while (!this.should_stop) {
+      const [[state, v], solved] = this.step(start_state, bound)
+
+      if (solved) return state
+
+      bound = v
+    }
+
+    return null
+  }
+}
+
+export namespace IterativeDeepeningAStar {
+  export type State<Underlying, MoveT> = {
+    parent: {
+      state: State<Underlying, MoveT>,
+      move: MoveT
+    } | null,
+    state: Underlying,
+    length: number,
+  }
+
+  export type Edge<Underlying, MoveT> = {
+    target: Underlying,
+    move: MoveT
+    cost: number
+  }
+
+  export namespace State {
+    export function moveSequence<A, B>(state: State<A, B>): B[] {
       if (!state.parent) return []
 
-      const par = reduce(state.parent.state)
+      const par = moveSequence(state.parent.state)
       par.push(state.parent.move)
       return par
     }
+  }
+}
 
-    const backlog: State[] = [{
-      state: self.start_state,
-      estimate: heuristic(self.start_state),
-      depth: 0,
-      blank: SliderState.blank(self.start_state),
-      parent: null
-    }]
+type SlideStateWithBlank = { tiles: SliderState, blank: number }
 
-    while (!self.should_stop) {
-      if (self.should_stop) break
+class IterativeDeepeningSlideSolver extends IterativeDeepeningAStar<SlideStateWithBlank, Move> {
+  constructor(start_state: SliderState, heurmod: number) {
+    super(
+      {tiles: start_state, blank: SliderState.blank(start_state)},
+      (n) => SliderState.sumManhattenDistance(n.tiles) * heurmod,
+      (node): IterativeDeepeningAStar.Edge<SlideStateWithBlank, Move>[] => {
+        const was_vertical = node.parent && node.parent.move % 5 == 0
+        const was_horizontal = node.parent && !was_vertical
 
-      let state = lodash.minBy(backlog, s => s.estimate)
+        const blank_x = node.state.blank % 5
+        const blank_y = Math.floor(node.state.blank / 5)
 
-      if (!state) break
+        const successors: IterativeDeepeningAStar.Edge<SlideStateWithBlank, Move>[] = []
 
-      while (!self.should_stop) {
-        if (best && state.depth > best.depth) break
-
-        if (is_goal(state)) {
-          found(state)
-          break;
+        function child(move: Move) {
+          const state = SliderState.withMove(node.state.tiles, move)
+          successors.push({
+            move: move,
+            cost: 1,
+            target: {
+              tiles: state,
+              blank: node.state.blank + move
+            }
+          })
         }
 
-        const succe = lodash.sortBy(successors(state), c => c.estimate)
+        if (!was_horizontal) {
+          for (let xi = 0; xi < 5; xi++) {
+            if (xi != blank_x) {
+              child(blank_y * 5 + xi - node.state.blank)
+            }
+          }
+        }
 
-        // Push half of the successors except the most promising to a backlog
-        //backlog.push(...succe.slice(1, succe.length / 2))
+        if (!was_vertical) {
+          for (let yi = 0; yi < 5; yi++) {
+            if (yi != blank_y) {
+              child(yi * 5 + blank_x - node.state.blank)
+            }
+          }
+        }
 
-        state = succe[0]
+        return successors // TODO: Exclude nodes already contained in the path?
+      },
+      (n) => SliderState.equals(SliderState.SOLVED, n.tiles)
+    );
+  }
+}
 
-        await self.checkTime()
+export class AStarSlideSolver extends Sliders.SlideSolver {
+
+  private subprocess: IterativeDeepeningSlideSolver
+
+  protected async solve_implementation() {
+    let factor = 3
+
+    while (!this.should_stop) {
+      this.subprocess = new IterativeDeepeningSlideSolver(this.start_state, 1 + factor)
+        .withTimeout(this.end_time - Date.now())
+        .onInterrupt(() => this.updateProgress())
+
+      console.log("Starting subprocess")
+
+      const result = await this.subprocess.run()
+
+      console.log("Subprocess ended")
+
+      if (result) {
+        this.registerSolution(IterativeDeepeningAStar.State.moveSequence(result))
       }
+
+      factor *= 0.9
+
+      this.checkTime()
     }
+  }
+
+  stop() {
+    super.stop()
+    this.subprocess?.stop()
   }
 }

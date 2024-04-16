0b9fde89920af15710ef8142c5fa6e7c46f4b30b
Lukas Gail <lukas.gail@outlook.com>
2024-04-16 13:00:42 +0200

    Add inverted analysis option

--- src/trainer/main.ts
+++ src/trainer/main.ts
@@ -179,6 +179,8 @@ class SliderAnalysisModal extends NisModal {
       name: string
     }
 
+    const ANALYSE_UNIQUENSS = false
+
     function analyse(dataset: DataSet): SliderAnalysis {
       const data = new Array(25).fill(0).map(() => new Array(25).fill(0))
       const counts: [SliderState, DataEntry[]][] = []
@@ -190,16 +192,18 @@ class SliderAnalysisModal extends NisModal {
           data[tile_id][position_id]++ // First index: The tile id, i.e., where the tile should be. Second index: Where the tile currently is
         })
 
-        const c_tuple = counts.find(([key, value]) => SliderState.equals(key, state.state))
+        if (ANALYSE_UNIQUENSS) {
+          const c_tuple = counts.find(([key, value]) => SliderState.equals(key, state.state))
 
-        if (c_tuple) c_tuple[1].push(state)
-        else counts.push([state.state, [state]])
+          if (c_tuple) c_tuple[1].push(state)
+          else counts.push([state.state, [state]])
+        }
       }
 
       return {
         original: dataset,
         tile_frequency: data,
-        counts: counts
+        counts: ANALYSE_UNIQUENSS ? counts : undefined
       }
     }
 
@@ -216,11 +220,19 @@ class SliderAnalysisModal extends NisModal {
     function render(layout: Properties, analysis: SliderAnalysis): Widget {
       const container = c()
 
+
+      const INVERTED = true
+
       layout.header(analysis.original.name)
-      layout.paragraph(`Analysis based on ${analysis.original.states.length} initial slider states containing ${analysis.counts?.length} unique states.`)
+      if (analysis.counts) {
+        layout.paragraph(`Analysis based on ${analysis.original.states.length} initial slider states containing ${analysis.counts?.length} unique states.`)
+      } else {
+        layout.paragraph(`Analysis based on ${analysis.original.states.length} initial slider states.`)
+      }
+
       layout.paragraph(`The outer 5 by 5 grid refers to where the respective tile belongs. The inner 5 by 5 grid shows where this tile is most likely to spawn.`)
 
-      if (analysis.counts.length != analysis.original.states.length) {
+      if (analysis.counts && analysis.counts.length != analysis.original.states.length) {
         for (let [state, duplicates] of analysis.counts.filter(c => c[1].length > 1)) {
           layout.header(state.toString(), "left", 1)
 
@@ -239,7 +251,9 @@ class SliderAnalysisModal extends NisModal {
         for (let should_x = 0; should_x < 5; should_x++) {
           const should_tile = should_y * 5 + should_x
 
-          const spawn_distribution = analysis.tile_frequency[should_tile]
+          const spawn_distribution = INVERTED
+            ? analysis.tile_frequency.map(t => t[should_tile])
+            : analysis.tile_frequency[should_tile]
 
           const outer_tile = c()
             .css2({
@@ -251,7 +265,7 @@ class SliderAnalysisModal extends NisModal {
 
           const frequencies = spawn_distribution.map(count => count / analysis.original.states.length)
 
-          const max_frequency = Math.max(...frequencies, 1/12)
+          const max_frequency = Math.max(...frequencies, 1 / 12)
 
           for (let is_y = 0; is_y < 5; is_y++) {
             const inner_row = hgrid().appendTo(outer_tile)

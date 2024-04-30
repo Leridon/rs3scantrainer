f77651e2bf38ff3d03f971f36e41f04f82554ce8
Lukas Gail <lukas.gail@outlook.com>
2024-04-30 12:09:23 +0200

    Trial implementation of lockbox solving algorithm

--- src/lib/cluetheory/Lockboxes.ts
+++ src/lib/cluetheory/Lockboxes.ts
@@ -1,7 +1,17 @@
+import * as lodash from "lodash";
+import {util} from "../util/util";
+import {Sliders} from "../../trainer/ui/neosolving/puzzles/Sliders";
+import {tiles} from "../../skillbertssolver/cluesolver/slidetiles";
+import {PathFinder} from "../runescape/movement";
+
 export namespace Lockboxes {
-
-
+  import todo = util.todo;
+  import Move = Sliders.Move;
+  import minimize = Lockboxes.MoveMap.minimize;
+  import state = PathFinder.state;
   export type Tile = 0 | 1 | 2
+
+  export const SIZE = 5
 
   export namespace Tile {
     export function advance(tile: Tile): Tile {
@@ -10,12 +20,182 @@ export namespace Lockboxes {
   }
 
   export type State = {
-    tiles: Tile[][]
+    tile_rows: Tile[][]
+  }
+
+  export type MoveMap = Tile[][]
+
+  export namespace MoveMap {
+    export const None: MoveMap = [
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+    ]
+
+    export const BASIC_QUIET_PATTERNS: MoveMap[] = [
+      [
+        [1, 0, 0, 0, 2],
+        [2, 2, 0, 1, 1],
+        [1, 2, 0, 1, 2],
+        [1, 1, 0, 2, 2],
+        [0, 2, 0, 1, 0]
+      ],
+      [
+        [1, 0, 1, 0, 0],
+        [2, 1, 2, 2, 0],
+        [2, 1, 0, 2, 1],
+        [1, 2, 1, 1, 0],
+        [1, 1, 2, 2, 1],
+      ],
+      [
+        [0, 1, 2, 2, 2],
+        [2, 0, 1, 0, 2],
+        [1, 2, 0, 1, 2],
+        [1, 0, 2, 0, 1],
+        [1, 1, 1, 2, 0],
+      ],
+      [
+        [1, 0, 2, 0, 1],
+        [2, 0, 1, 0, 2],
+        [0, 0, 0, 0, 0],
+        [1, 0, 2, 0, 1],
+        [2, 0, 1, 0, 2],
+      ], [
+        [1, 1, 0, 2, 2],
+        [1, 1, 0, 2, 2],
+        [0, 0, 0, 0, 0],
+        [2, 2, 0, 1, 1],
+        [2, 2, 0, 1, 1],
+      ]
+    ]
+
+    export function get(moves: MoveMap, row: number, col: number): Tile {
+      if (row < 0 || row >= SIZE || col < 0 || col >= SIZE) return 0
+      else return moves[row][col]
+    }
+
+    export function chain(...moves: MoveMap[]): MoveMap {
+      return None.map((row, row_i) => {
+        return row.map((_, col_i) => {
+          return lodash.sumBy(moves, m => m[row_i][col_i]) % 3 as Tile
+        })
+      })
+    }
+
+    export function cost(map: MoveMap): number {
+      return lodash.sum(map.flat())
+    }
+
+    export function isQuiet(map: MoveMap): boolean {
+      return State.equals(State.applyMoves(State.SOLVED, map), State.SOLVED)
+    }
+
+    export function allQuietPatterns(): MoveMap[] {
+      return [None, ...BASIC_QUIET_PATTERNS] // TODO: Quiet pattern recombinations
+    }
+
+    export function minimize(map: MoveMap): MoveMap {
+      return lodash.minBy(allQuietPatterns().map(quiet => chain(map, quiet)), cost)
+    }
   }
 
   export namespace State {
-    export function applyMove(state: State, move: [number, number]): void {
-      
+    export const SOLVED: State = {
+      tile_rows: [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+      ]
     }
+
+    /**
+     * A lockbox is solved if all tiles have the same value
+     */
+    export function isSolved(state: State): boolean {
+      const ref = state.tile_rows[0][0]
+      return state.tile_rows.every(r => r.every(t => t == ref))
+    }
+
+    export function equals(a: State, b: State): boolean {
+      for (let col = 0; col < 5; col++) {
+        for (let row = 0; row < 5; row++) {
+          if (a.tile_rows[row][col] != b.tile_rows[row][col]) return false
+        }
+      }
+      return true
+    }
+
+    export function applyMoves(state: State, move: MoveMap): State {
+      return {
+        tile_rows: state.tile_rows.map((row, row_i) =>
+          row.map((tile, col_i) =>
+            (
+              tile
+              + MoveMap.get(move, row_i, col_i)
+              + MoveMap.get(move, row_i + 1, col_i)
+              + MoveMap.get(move, row_i - 1, col_i)
+              + MoveMap.get(move, row_i, col_i + 1)
+              + MoveMap.get(move, row_i, col_i - 1)
+            ) % 3 as Tile
+          )
+        )
+      }
+    }
+  }
+
+  export function solve(state: State): MoveMap {
+
+    function chasing(s: State): [MoveMap, State] {
+      let moves = MoveMap.None
+
+      for (let chased_row = 0; chased_row < 4; chased_row++) {
+        const partial = MoveMap.None.map((row, row_i) => {
+          if (row_i == chased_row + 1) return s.tile_rows[chased_row]
+          else return row
+        })
+
+        s = State.applyMoves(s, partial)
+
+        moves = MoveMap.chain(moves, partial)
+      }
+
+      return [moves, s]
+    }
+
+    const [first_chasing_moves, state_after_first_chasing] = chasing(state)
+
+    const B1 = [0, 2, 1][state_after_first_chasing[4][0]] as Tile// If the light at A5 is red then press B1 twice, if it is green press it once.
+    const A1B1 = [0, 2, 1][state_after_first_chasing[4][1]] as Tile// If the light at B5 is red then press A1 and B1 twice, if it is green press them once.
+
+    const magic_solve: MoveMap = [
+      [A1B1, (A1B1 + B1) % 3 as Tile, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+    ]
+
+    const state_after_magic_solve = State.applyMoves(state_after_first_chasing, magic_solve)
+
+    const [second_chasing_moves, state_after_second_chasing] = chasing(state_after_magic_solve)
+
+    const total_solution = MoveMap.chain(first_chasing_moves, magic_solve, second_chasing_moves)
+
+    return MoveMap.minimize(total_solution)
+  }
+
+  export function solveAnyTarget(state: State): MoveMap {
+    const candidate_targets = [
+      state,
+      ...[1, 2].map(o => ({
+        tile_rows: state.tile_rows.map(row => row.map(t => (t + o) % 3 as Tile))
+      }))
+    ]
+
+    return lodash.minBy(candidate_targets.map(solve), MoveMap.cost)
   }
 }

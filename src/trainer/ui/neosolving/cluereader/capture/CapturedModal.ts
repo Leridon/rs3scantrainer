1bb85fa5f7a1ed3ab93433d1f8c48848f01c0c88
Lukas Gail <lukas.gail@outlook.com>
2024-05-12 17:02:14 +0200

    Fix modal reader for legacy

--- src/trainer/ui/neosolving/cluereader/capture/CapturedModal.ts
+++ src/trainer/ui/neosolving/cluereader/capture/CapturedModal.ts
@@ -3,7 +3,8 @@ import * as OCR from "@alt1/ocr";
 import {Vector2} from "../../../../../lib/math";
 import {LazyAsync} from "../../../../../lib/properties/Lazy";
 import {ImageDetect} from "@alt1/base";
-import {CapturedImage, imageSize} from "../../../../../lib/alt1/ImageCapture";
+import {CapturedImage} from "../../../../../lib/alt1/ImageCapture";
+import {OverlayGeometry} from "../../../../../lib/alt1/OverlayGeometry";
 
 export class CapturedModal {
   private _title: string = null
@@ -15,14 +16,30 @@ export class CapturedModal {
 
   title(): string {
     if (!this._title && this.body.parent) {
-      const TITLE_BAR_OFFSET_FROM_BODY = {x: 0, y: -24}
-      const TITLE_BAR_SIZE = {x: 250, y: 20}
+      if(this.isLegacy) {
+        const TITLE_BAR_OFFSET_FROM_BODY = {x: 0, y: -24}
+        const TITLE_BAR_SIZE = {x: 500, y: 20}
 
-      const title_bar = this.body.parent.getSubSection(
-        ScreenRectangle.move(this.body.relativeRectangle(), TITLE_BAR_OFFSET_FROM_BODY, TITLE_BAR_SIZE)
-      ).getData()
+        const title_bar = this.body.parent.getSubSection(
+          ScreenRectangle.move(this.body.relativeRectangle(),
+            TITLE_BAR_OFFSET_FROM_BODY,
+            TITLE_BAR_SIZE
+          )
+        ).getData()
 
-      this._title = OCR.readSmallCapsBackwards(title_bar, CapturedModal.title_font, [[255, 203, 5]], 0, 13, title_bar.width, 1).text;
+        this._title = OCR.readSmallCapsBackwards(title_bar, CapturedModal.title_font, [[255, 152, 31]], 0, 13, title_bar.width, 1).text
+
+        console.log(this._title)
+      } else {
+        const TITLE_BAR_OFFSET_FROM_BODY = {x: 0, y: -24}
+        const TITLE_BAR_SIZE = {x: 250, y: 20}
+
+        const title_bar = this.body.parent.getSubSection(
+          ScreenRectangle.move(this.body.relativeRectangle(), TITLE_BAR_OFFSET_FROM_BODY, TITLE_BAR_SIZE)
+        ).getData()
+
+        this._title = OCR.readSmallCapsBackwards(title_bar, CapturedModal.title_font, [[255, 203, 5]], 0, 13, title_bar.width, 1).text;
+      }
     }
 
     return this._title.toString()
@@ -40,23 +57,25 @@ export class CapturedModal {
 
       if (!x) continue
 
-      const top_left = img.getSubSection(
-        ScreenRectangle.move(x.relativeRectangle(), skin.TL_OFFSET_FROM_X, imageSize(skin.top_left))
-      ).find(skin.top_left)[0]
+      console.log("X found")
 
-      if (!top_left) return null;
+      const top_left = img.find(skin.top_left)[0]
+      if (!top_left) {
+        console.log("Couldnt find top left")
+        return null;
+      }
 
-      const bot_left = img.getSubSection(
-        ScreenRectangle.move(x.relativeRectangle(), skin.BL_OFFSET_FROM_X, imageSize(skin.bot_left))
-      ).find(skin.bot_left)[0]
-
-      if (!bot_left) return null;
+      const bot_left = img.find(skin.bot_left)[0]
+      if (!bot_left) {
+        console.log("Couldnt find bot left")
+        return null;
+      }
 
       const body_tl = Vector2.add(top_left.relativeRectangle().origin, skin.BODY_TL_OFFSET_FROM_TL)
       const body_bl = Vector2.add(bot_left.relativeRectangle().origin, skin.BODY_BL_OFFSET_FROM_BL)
       const body_tr = Vector2.add(x.relativeRectangle().origin, skin.BODY_TR_OFFSET_FROM_X)
 
-      const body_height = (bot_left.relativeRectangle().origin.y + skin.BODY_BL_OFFSET_FROM_BL.y) - (top_left.relativeRectangle().origin.y + skin.BODY_TL_OFFSET_FROM_TL.y) + 1
+      const body_height = body_bl.y - body_tl.y + 1
       const body_width = body_tr.x - body_tl.x + 1
 
       const BODY_SIZE: Vector2 = {x: body_width, y: body_height}
@@ -84,8 +103,6 @@ export namespace CapturedModal {
     BODY_TL_OFFSET_FROM_TL: Vector2
     BODY_BL_OFFSET_FROM_BL: Vector2,
     BODY_TR_OFFSET_FROM_X: Vector2,
-    TL_OFFSET_FROM_X: Vector2,
-    BL_OFFSET_FROM_X: Vector2
   }
 
   export const anchors = new LazyAsync<SkinAnchors[]>(async () => {
@@ -97,15 +114,17 @@ export namespace CapturedModal {
 
       BODY_TL_OFFSET_FROM_TL: {x: 4, y: 29},
       BODY_BL_OFFSET_FROM_BL: {x: 4, y: 7},
-      BODY_TR_OFFSET_FROM_X: {x: -20, y: -24},
-      TL_OFFSET_FROM_X: {x: -479, y: -5},
-      BL_OFFSET_FROM_X: {x: -479, y: 328},
-    },/* {
+      BODY_TR_OFFSET_FROM_X: {x: 20, y: 24},
+    }, {
       isLegacy: true,
       close_x: await ImageDetect.imageDataFromUrl("alt1anchors/legacyx.png"),
       top_left: await ImageDetect.imageDataFromUrl("alt1anchors/legacytopleft.png"),
-      bot_left: await ImageDetect.imageDataFromUrl("alt1anchors/legacybotleft.png")
-    },*/
+      bot_left: await ImageDetect.imageDataFromUrl("alt1anchors/legacybotleft.png"),
+
+      BODY_TL_OFFSET_FROM_TL: {x: 4, y: 29},
+      BODY_BL_OFFSET_FROM_BL: {x: 6, y: -2},
+      BODY_TR_OFFSET_FROM_X: {x: 19, y: 20},
+    },
     ]
   })
 }

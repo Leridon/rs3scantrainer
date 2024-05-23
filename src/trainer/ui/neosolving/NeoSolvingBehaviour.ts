9593b3a82c388bee404b63d6b778f0bfd43a6057
Lukas Gail <lukas.gail@outlook.com>
2024-05-23 23:26:13 +0200

    Fix null pointer exception

--- src/trainer/ui/neosolving/NeoSolvingBehaviour.ts
+++ src/trainer/ui/neosolving/NeoSolvingBehaviour.ts
@@ -740,7 +740,7 @@ export default class NeoSolvingBehaviour extends Behaviour {
         this.app.settings.update(set => set.teleport_customization.active_preset = bound_preset)
     }
 
-    if (read_result.type == "compass" && clue.type == "compass") {
+    if (read_result?.type == "compass" && clue.type == "compass") {
       const behaviour = new CompassSolving(this, clue, read_result.reader)
 
       behaviour.selected_spot.subscribe(async spot => {

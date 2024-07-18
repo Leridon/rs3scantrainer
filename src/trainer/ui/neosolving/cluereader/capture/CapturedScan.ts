0a6843720c9a9b34abffac3886144541702643ba
Lukas Gail <lukas.gail@outlook.com>
2024-07-18 18:52:50 +0200

    Extract various additional information from the scan panel

--- src/trainer/ui/neosolving/cluereader/capture/CapturedScan.ts
+++ src/trainer/ui/neosolving/cluereader/capture/CapturedScan.ts
@@ -4,6 +4,9 @@ import {ImageDetect} from "@alt1/base";
 import {Lazy, lazy, LazyAsync} from "../../../../../lib/properties/Lazy";
 import {ScreenRectangle} from "../../../../../lib/alt1/ScreenRectangle";
 import * as OCR from "@alt1/ocr";
+import {util} from "../../../../../lib/util/util";
+import index = util.index;
+import stringSimilarity = util.stringSimilarity;
 
 export class CapturedScan {
 
@@ -27,7 +30,7 @@ export class CapturedScan {
 
     for (let lineindex = 0; lineindex < 13; lineindex++) {
       const y = lineindex * LINEHEIGHT;
-      const line = OCR.findReadLine(data, font, [[255, 255, 255]], 70, y, 40, 1);
+      const line = OCR.findReadLine(data, font, [[255, 255, 255], [239, 176, 99], [192, 192, 192], [255, 223, 0]], 70, y, 40, 1);
       lines.push(line.text);
     }
 
@@ -52,12 +55,48 @@ export class CapturedScan {
     return cleaned_lines
   })
 
+  private _different_level: Lazy<boolean> = lazy(() => {
+    const line = this._lines.get()[1]
+
+    const similarity = stringSimilarity(line, "Try scanning a different level.")
+
+    return similarity > 0.7
+  })
+
+  private _meerkats_active = lazy((): boolean | "unknown" => {
+    const last_line = index(this._lines.get(), -1)
+
+    const similarity = stringSimilarity(last_line, "Your meerkats are increasing your scan range by")
+
+    return similarity > 0.7
+  })
+
+  private _triple = lazy(() => {
+    const line = this._lines.get()[1]
+
+    const similarity = stringSimilarity(line, "The orb glows as you scan. You are in range of the coordinate! The coordinate is")
+
+    return similarity > 0.7
+  })
+
   text(): string {
     return this._lines.get().join("\n")
   }
 
   scanArea(): string {
     return this._lines.get()[0]
+  }
+
+  hasMeerkats(): boolean | "unknown" {
+    return this._meerkats_active.get()
+  }
+
+  isDifferentLevel(): boolean {
+    return this._different_level.get()
+  }
+
+  isTriple(): boolean {
+    return this._triple.get()
   }
 
   static async find(screen: CapturedImage): Promise<CapturedScan> {

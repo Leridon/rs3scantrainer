474904cf42aa5b44a6cca914ec55630a3a7940a7
Lukas Gail <lukas.gail@outlook.com>
2024-10-08 20:24:35 +0200

    First version of broadcast reader frontend

--- src/lib/ui/controls/TextField.ts
+++ src/lib/ui/controls/TextField.ts
@@ -1,9 +1,14 @@
 import AbstractEditWidget from "trainer/ui/widgets/AbstractEditWidget";
 import * as jquery from "jquery";
+import {ewent, EwentHandler} from "../../reactive";
 
 export default class TextField extends AbstractEditWidget<string> {
-  constructor() {
-    super(jquery("<input type='text' class='nisl-textinput'>"));
+  private confirmed = ewent<string>()
+
+  constructor(is_password: boolean = false) {
+    super(jquery("<input class='nisl-textinput'>"));
+
+    this.setAttribute("type", is_password ? "password" : "text")
 
     this.container
       .val(this.get())
@@ -12,6 +17,11 @@ export default class TextField extends AbstractEditWidget<string> {
       })
       .on("change", () => {
         this.commit(this.container.val() as string)
+      })
+      .on("keydown", (e) => {
+        if (e.key == "Enter") {
+          this.confirmed.trigger(this.get())
+        }
       })
   }
 
@@ -22,6 +32,12 @@ export default class TextField extends AbstractEditWidget<string> {
 
   setPlaceholder(placeholder: string): this {
     this.container.attr("placeholder", placeholder)
+
+    return this
+  }
+
+  onConfirm(f: (_: string) => void, handle: (_: EwentHandler<any>) => void = () => {}): this {
+    handle(this.confirmed.on(f))
 
     return this
   }

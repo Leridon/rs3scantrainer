fca7d9196b56aaf451e22ce69abf5673bacd6253
Lukas Gail <lukas.gail@outlook.com>
2024-10-08 20:39:25 +0200

    Fix inconistency with chat timestamps causing repeated reads of the same message

--- src/lib/alt1/readers/ChatReader.ts
+++ src/lib/alt1/readers/ChatReader.ts
@@ -258,6 +258,7 @@ export namespace ChatReader {
       today.setHours(hours)
       today.setMinutes(minutes)
       today.setSeconds(seconds)
+      today.setMilliseconds(0)
 
       const date = lodash.minBy([today, addDays(today, -1), addDays(today, 1)], date => Math.abs(now - date.valueOf()))
 
